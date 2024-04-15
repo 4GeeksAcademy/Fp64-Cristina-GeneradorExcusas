8bcf14c48adbbb911d13ec28d744fb83a5cac524
Cristinacbm5 <157827345+Cristinacbm5@users.noreply.github.com>
2024-04-15 00:34:30 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,29 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+function generarArgumento() {
+  let quien = ["Canarias", "Europa", "España"];
+  let action = ["está explotada", "sufre"];
+  let what = ["por los turistas", "por el petróleo"];
+  let when = ["todos los años", "en verano", "en temporada alta"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  // Seleccionar una palabra aleatoria de cada uno de los arrays
+  let quienSeleccionado = quien[Math.floor(Math.random() * quien.length)];
+  let actionSeleccionada = action[Math.floor(Math.random() * action.length)];
+  let whatSeleccionado = what[Math.floor(Math.random() * what.length)];
+  let whenSeleccionado = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+  // Construir la frase
+  let argumento =
+    quienSeleccionado +
+    " " +
+    actionSeleccionada +
+    " " +
+    whatSeleccionado +
+    " " +
+    whenSeleccionado +
+    ".";
+
+  // Actualizar el contenido del elemento HTML con id "argumento"
+  document.getElementById("argumento").textContent = argumento;
+}
+
+// Llamar a la función para generar la excusa cuando la ventana esté completamente cargada
+window.onload = generarArgumento;
