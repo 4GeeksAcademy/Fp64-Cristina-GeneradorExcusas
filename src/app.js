3fe39be56324b217ede73ae861bb82f12a8bf209
Cristinacbm5 <157827345+Cristinacbm5@users.noreply.github.com>
2024-04-15 00:42:59 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -4,13 +4,11 @@ function generarArgumento() {
   let what = ["por los turistas", "por el petróleo"];
   let when = ["todos los años", "en verano", "en temporada alta"];
 
-  // Seleccionar una palabra aleatoria de cada uno de los arrays
   let quienSeleccionado = quien[Math.floor(Math.random() * quien.length)];
   let actionSeleccionada = action[Math.floor(Math.random() * action.length)];
   let whatSeleccionado = what[Math.floor(Math.random() * what.length)];
   let whenSeleccionado = when[Math.floor(Math.random() * when.length)];
 
-  // Construir la frase
   let argumento =
     quienSeleccionado +
     " " +
@@ -21,9 +19,7 @@ function generarArgumento() {
     whenSeleccionado +
     ".";
 
-  // Actualizar el contenido del elemento HTML con id "argumento"
-  document.getElementById("argumento").textContent = argumento;
+  document.getElementoPorId("argumento").textContent = argumento;
 }
 
-// Llamar a la función para generar la excusa cuando la ventana esté completamente cargada
 window.onload = generarArgumento;
